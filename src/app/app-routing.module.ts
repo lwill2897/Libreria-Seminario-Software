20eb65c17fd4235b77d75fd2c485b1018cc5f1e9
lwill2897 <51091301+lwill2897@users.noreply.github.com>
2020-11-23 19:23:20 -0600

    Modificando el app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+import { InicioComponent } from './inicio/inicio.component';
+import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
+
+const routes: Routes = [
+  {path:'inicio', component: InicioComponent},
+  {path:'**', redirectTo:'inicio', pathMatch:'full'},
+  {path:'iniciosesion', component: IniciosesionComponent},
+  {path:'**', redirectTo:'iniciosesion', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
